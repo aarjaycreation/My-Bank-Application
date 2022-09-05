cebdd09656fc8273600583d04f887cb3f01893b7
Rahul <aarjayjangir@gmail.com>
2022-09-05 16:07:23 +0530

    Add project files.

---
+++ MyBankApp/BankAccount.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBankApp
+{
+    public  class BankAccount
+    {
+
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance = balance + item.Amount;
+                    //balance += item.Amount;
+                }
+
+                return balance;
+            }
+        }
+
+
+        private static int accountNumber = 1234567890;
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        // constructor
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Number = accountNumber.ToString();
+            accountNumber++;
+
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+        }
+        //Deposit
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+        //Withdrawal
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+
+    }
+}
---
+++ MyBankApp/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using MyBankApp;
+
+var account = new BankAccount("rahul", 10000);
+var newAccount = new BankAccount("satyam", 5000);
+//Console.WriteLine(account.Owner);
+Console.WriteLine($"Account [{account.Number}] was created for [{account.Owner}] with [{account.Balance}] initial balance.");
+//new account 
+Console.WriteLine($"Account [{newAccount.Number}] was created for [{newAccount.Owner}] with [{newAccount.Balance}] initial balance.");
+
+
+
+account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+Console.WriteLine($"your current balance = {account.Balance}");
+account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+Console.WriteLine($"your current balance = {account.Balance}");
+
+//Test that the initial balances must be positive.
+BankAccount invalidAccount;
+try
+{
+    invalidAccount = new BankAccount("invalid", -55);
+}
+catch (ArgumentOutOfRangeException e)
+{
+    Console.WriteLine("Exception caught creating account with negative balance");
+    Console.WriteLine(e.ToString());
+    return;
+}
+
+
+// Test for a negative balance.
+try
+{
+    account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+}
+catch (InvalidOperationException e)
+{
+    Console.WriteLine("Exception caught trying to overdraw");
+    Console.WriteLine(e.ToString());
+}
